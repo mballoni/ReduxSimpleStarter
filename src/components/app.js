b8c2eba24af050da0cb1eec142edddf7c792dd97
marceloballoni <marceloballoni@gmail.com>
2018-04-18 17:32:53 -0300

    Basic redux learning

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import BookList from "../containers/BookList";
+import BookDetail from "../containers/BookDetail"
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <BookList/>
+                <BookDetail/>
+            </div>
+        );
+    }
 }
